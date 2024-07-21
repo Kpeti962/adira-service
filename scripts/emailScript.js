099f847ca7d5b52087530f4990e2ccca3a6c5f2d
Koncz Péter <kpeti962@gmail.com>
2024-07-21 20:28:27 +0200

    -

--- scripts/emailScript.js
+++ scripts/emailScript.js
@@ -6,5 +6,11 @@ function sendMail() {
         message: document.getElementById("message").value,
     }
 
-    emailjs.send("service_vochazd", "template_q5cwc7b", params).then(alert("Email elküldve!"));
+/*     emailjs.send("service_vochazd", "template_q5cwc7b", params).then(alert("Email elküldve!"));
+ */
+    subject.value = "";
+    name.value = "";
+    email.value = "";
+    message.value = "";
+
 }
