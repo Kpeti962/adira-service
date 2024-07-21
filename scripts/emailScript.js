2e2688323ea56ba3b5241200587f1f793dbf2913
Koncz Péter <kpeti962@gmail.com>
2024-07-21 19:01:13 +0200

    emailjs made

---
+++ scripts/emailScript.js
@@ -0,0 +1,10 @@
+function sendMail() {
+    let params = {
+        subject: document.getElementById("options").value,
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    }
+
+    emailjs.send("service_vochazd", "template_q5cwc7b", params).then(alert("Email elküldve!"));
+}
