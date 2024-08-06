d8982a7f4125fe06f867eb2a45cabe11233cc386
Koncz Péter <kpeti962@gmail.com>
2024-08-06 14:51:44 +0200

    ajanlatkeres optimizalasa

--- scripts/emailScript.js
+++ scripts/emailScript.js
@@ -1,36 +1,56 @@
 function sendMail() {
 
-const wrongAlert = document.querySelector(".wrongAlert");
-const successAlert = document.querySelector(".successAlert");
+    const wrongAlert = document.querySelector(".wrongAlert");
+    const successAlert = document.querySelector(".successAlert");
     const params = {
         subject: document.getElementById("options").value,
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         message: document.getElementById("message").value
+
     };
-    
+
     let datasheet = document.getElementById("checkbox").checked;
-    
+
     if (datasheet && params.subject !== "" && params.email !== "" && params.message !== "") {
         emailjs.send("service_vochazd", "template_q5cwc7b", params)
-        .then(() => {
-            successAlert.classList.remove("hidden");
-            setTimeout(() => {
-                successAlert.classList.add("hidden");
-            }, 3000);
-        });
+            .then(() => {
+                successAlert.classList.remove("hidden");
+                setTimeout(() => {
+                    successAlert.classList.add("hidden");
+                }, 3000);
+            });
 
         document.getElementById("options").value = "";
         document.getElementById("name").value = "";
         document.getElementById("email").value = "";
         document.getElementById("message").value = "";
         document.getElementById("checkbox").checked = false;
-    } else if(!datasheet){
+    } else if (!datasheet) {
         wrongAlert.classList.remove("hidden")
-        
+
         setTimeout(() => {
-             wrongAlert.classList.add("hidden")
-          }, 3000);
+            wrongAlert.classList.add("hidden")
+        }, 3000);
     }
 
 }
+
+
+function getQueryParams() {
+    const params = {};
+    window.location.search.substring(1).split("&").forEach(pair => {
+        const [key, value] = pair.split("=");
+        params[decodeURIComponent(key)] = decodeURIComponent(value);
+    });
+    return params;
+}
+
+window.onload = function () {
+    const params = getQueryParams();
+    if (params.selected) {
+        const select = document.getElementById("options");
+        select.value = params.selected;
+    }
+};
+
