e010a5b7b5d88b796a377230c7bfef612cf67956
Koncz Péter <kpeti962@gmail.com>
2024-08-09 14:54:13 +0200

    -

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,9 @@ const thamnav = document.querySelector('.thamnav');
 const body = document.querySelector('.body');
 const overlay = document.querySelector('.overlay');
 const arrowUp = document.querySelector('.arrowUp');
+const serviceButton = document.querySelector('.serviceButton');
+const serviceButton2 = document.querySelector('.serviceButton2');
+const servicePage = document.querySelector('.servicePage');
 
 
 tham.addEventListener('click', () => {
@@ -17,11 +20,23 @@ overlay.addEventListener('click', () => {
   body.classList.toggle('overflow-hidden');
 });
 
+serviceButton.addEventListener('click', () => {
+  servicePage.scrollIntoView({behavior: "smooth"});
+  console.log("siker");
+  
+})
+serviceButton2.addEventListener('click', () => {
+  thamnav.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+  body.classList.toggle('overflow-hidden');
+  servicePage.scrollIntoView({behavior: "smooth"});
+  console.log("siker");
+  
+})
 arrowUp.addEventListener('click', () => {
 window.scrollTo({top: 0, behavior: "smooth"})
 
 
 
-
-});
+}); 
 
