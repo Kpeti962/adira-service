c3336eeb9fc9e2082e1ebaaad4ef55b7c7cca3c9
Koncz Péter <kpeti962@gmail.com>
2024-08-15 18:30:03 +0200

    table finsihed

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,9 @@ const body = document.querySelector('.body');
 const overlay = document.querySelector('.overlay');
 const arrowUp = document.querySelector('.arrowUp');
 const nav = document.querySelector('.nav');
-const serviceButton = document.querySelector('.serviceButton');
 const contactButton = document.querySelector('.contactButton');
-const serviceButton2 = document.querySelector('.serviceButton2');
+
 const contactButton2 = document.querySelector('.contactButton2');
-const servicePage = document.querySelector('.servicePage');
 const contactPage = document.querySelector('.contactPage');
 
 
@@ -24,23 +22,19 @@ overlay.addEventListener('click', () => {
   body.classList.toggle('overflow-hidden');
 });
 
-serviceButton.addEventListener('click', () => {
-  servicePage.scrollIntoView({behavior: "smooth"});
-  console.log("siker");
-  
-})
-contactButton.addEventListener('click', () => {
-  contactPage.scrollIntoView({behavior: "smooth"});
-  
-  
-})
+
 serviceButton2.addEventListener('click', () => {
   thamnav.classList.toggle('hidden');
   overlay.classList.toggle('hidden');
   body.classList.toggle('overflow-hidden');
   servicePage.scrollIntoView({behavior: "smooth"});
   
-
+  
+})
+contactButton.addEventListener('click', () => {
+  contactPage.scrollIntoView({behavior: "smooth"});
+  
+  
 })
 contactButton2.addEventListener('click', () => {
   thamnav.classList.toggle('hidden');
