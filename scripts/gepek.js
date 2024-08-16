90257ca7067edeb79765fed0f4506d6056810c12
Koncz Péter <kpeti962@gmail.com>
2024-08-16 15:47:25 +0200

    -

--- scripts/gepek.js
+++ scripts/gepek.js
@@ -3,14 +3,14 @@ const extraRows = document.querySelectorAll(".extraRows");
 const toggleRows = document.querySelector(".toggleRows");
 
 
-arrowUp.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: "smooth" })
-}); 
-
 toggleRows.addEventListener("click", () => {
     extraRows.forEach(row => row.classList.remove("hidden"));
     toggleRows.classList.add("hidden");
 });
+arrowUp.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+}); 
+
     
     
   
